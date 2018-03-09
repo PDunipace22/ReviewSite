f4d0d228b40ec5b90f4bcc4c08f02774d1b554b8
PDunipace22 <peterdunipace@gmail.com>
2018-03-09 12:53:16 -0500

    created models and controllers

---
+++ ReviewSite/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReviewSite.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        [Display(Name ="Category")]
+        public string Name { get; set; }
+        //Navigation Property
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ ReviewSite/Models/Review.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ReviewSite.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        public string Title { get; set; }
+        public int Rating { get; set; }
+        public string Author { get; set; }
+        [Display(Name ="Date Published")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        public DateTime PublishedDate { get; set; }
+        public string Content { get; set; }
+        
+        //Foreign Key and Navigation Property
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
